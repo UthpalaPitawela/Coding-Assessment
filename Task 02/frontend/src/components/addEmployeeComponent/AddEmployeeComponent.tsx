c43beaaf5628eaf82ef313b1a42ed2b554a962ec
UthpalaPitawela <uthpala.isiru@gmail.com>
2024-05-06 00:36:02 +0530

    pages link done for Task 02

--- Task 02/frontend/src/components/addEmployeeComponent/AddEmployeeComponent.tsx
+++ Task 02/frontend/src/components/addEmployeeComponent/AddEmployeeComponent.tsx
@@ -4,10 +4,12 @@ import { useContext, useState } from "react";
 import { EmployeeContext } from "../../context/employeeContext";
 import { EmployeeContextType, IEmployee } from "../../types/employee.types";
 import { addEmployee } from "../../services/employeeService";
+import { EmployeePage } from "../../pages/EmployeePage";
 
 export const AddEmployeeComponent = () => {
   const [loading, setLoading] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loadEmployeeList, setLoadEmployeeList] = useState(false);
   const [alert, setAlert] = useState("");
 
   const { employees, setEmployees } = useContext(
@@ -39,130 +41,161 @@ export const AddEmployeeComponent = () => {
   };
 
   return (
-    <div className="container">
-      <div className="list-emp-btn">
-        <button className="btn btn-primary">List Employees</button>
-      </div>
-      <div className="row">
-        <h3>Add Employee</h3>
-      </div>
-      <div className="row">
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label" htmlFor="id">
-              Employee Id
-            </label>
-            <div className="col-sm-10">
-              <input
-                id="id"
-                className="form-control"
-                aria-invalid={errors.employeeId ? "true" : "false"}
-                {...register("employeeId", {
-                  required: { value: true, message: "Employee Id is required" },
-                })}
-                type="text"
-              />
-              {errors.employeeId && (
-                <span role="alert">{errors.employeeId.message as string}</span>
-              )}
+    <>
+      {loadEmployeeList ? (
+        <EmployeePage />
+      ) : (
+        <>
+          <div className="container">
+            <div className="list-emp-btn">
+              <button
+                className="btn btn-primary"
+                onClick={() => setLoadEmployeeList(true)}
+              >
+                List Employees
+              </button>
             </div>
-          </div>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label" htmlFor="name">
-              Name
-            </label>
-            <div className="col-sm-10">
-              <input
-                id="name"
-                className="form-control"
-                aria-invalid={errors.name ? "true" : "false"}
-                {...register("name", {
-                  required: { value: true, message: "Name is required" },
-                })}
-                type="text"
-              />
-              {errors.name && (
-                <span role="alert">{errors.name.message as string}</span>
-              )}
+            <div className="row">
+              <h3>Add Employee</h3>
             </div>
-          </div>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label" htmlFor="designation">
-              Designation
-            </label>
-            <div className="col-sm-10">
-              <input
-                id="name"
-                className="form-control"
-                aria-invalid={errors.designation ? "true" : "false"}
-                {...register("designation", {
-                  required: { value: true, message: "Designation is required" },
-                })}
-                type="text"
-              />
-              {errors.designation && (
-                <span role="alert">{errors.designation.message as string}</span>
-              )}
-            </div>
-          </div>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label" htmlFor="age">
-              Age
-            </label>
-            <div className="col-sm-10">
-              <input
-                id="age"
-                className="form-control"
-                aria-invalid={errors.age ? "true" : "false"}
-                {...register("age", {
-                  required: { value: true, message: "Age is required" },
-                })}
-                type="number"
-              />
-              {errors.age && (
-                <span role="alert">{errors.age.message as string}</span>
-              )}
-            </div>
-          </div>
-          <div className="form-group row">
-            <label className="col-sm-2 col-form-label" htmlFor="location">
-              Location
-            </label>
-            <div className="col-sm-10">
-              <input
-                id="location"
-                className="form-control"
-                aria-invalid={errors.location ? "true" : "false"}
-                {...register("location", {
-                  required: { value: true, message: "Location is required" },
-                })}
-                type="text"
-              />
-              {errors.location && (
-                <span role="alert">{errors.location.message as string}</span>
-              )}
-            </div>
-          </div>
+            <div className="row">
+              <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label" htmlFor="id">
+                    Employee Id
+                  </label>
+                  <div className="col-sm-10">
+                    <input
+                      id="id"
+                      className="form-control"
+                      aria-invalid={errors.employeeId ? "true" : "false"}
+                      {...register("employeeId", {
+                        required: {
+                          value: true,
+                          message: "Employee Id is required",
+                        },
+                      })}
+                      type="text"
+                    />
+                    {errors.employeeId && (
+                      <span role="alert">
+                        {errors.employeeId.message as string}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label" htmlFor="name">
+                    Name
+                  </label>
+                  <div className="col-sm-10">
+                    <input
+                      id="name"
+                      className="form-control"
+                      aria-invalid={errors.name ? "true" : "false"}
+                      {...register("name", {
+                        required: { value: true, message: "Name is required" },
+                      })}
+                      type="text"
+                    />
+                    {errors.name && (
+                      <span role="alert">{errors.name.message as string}</span>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label
+                    className="col-sm-2 col-form-label"
+                    htmlFor="designation"
+                  >
+                    Designation
+                  </label>
+                  <div className="col-sm-10">
+                    <input
+                      id="name"
+                      className="form-control"
+                      aria-invalid={errors.designation ? "true" : "false"}
+                      {...register("designation", {
+                        required: {
+                          value: true,
+                          message: "Designation is required",
+                        },
+                      })}
+                      type="text"
+                    />
+                    {errors.designation && (
+                      <span role="alert">
+                        {errors.designation.message as string}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label" htmlFor="age">
+                    Age
+                  </label>
+                  <div className="col-sm-10">
+                    <input
+                      id="age"
+                      className="form-control"
+                      aria-invalid={errors.age ? "true" : "false"}
+                      {...register("age", {
+                        required: { value: true, message: "Age is required" },
+                      })}
+                      type="number"
+                    />
+                    {errors.age && (
+                      <span role="alert">{errors.age.message as string}</span>
+                    )}
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-2 col-form-label" htmlFor="location">
+                    Location
+                  </label>
+                  <div className="col-sm-10">
+                    <input
+                      id="location"
+                      className="form-control"
+                      aria-invalid={errors.location ? "true" : "false"}
+                      {...register("location", {
+                        required: {
+                          value: true,
+                          message: "Location is required",
+                        },
+                      })}
+                      type="text"
+                    />
+                    {errors.location && (
+                      <span role="alert">
+                        {errors.location.message as string}
+                      </span>
+                    )}
+                  </div>
+                </div>
 
-          {loading ? (
-            <button className="btn btn-primary" type="button" disabled>
-              <span
-                className="spinner-border spinner-border-sm"
-                role="status"
-                aria-hidden="true"
-              ></span>
-              Loading...
-            </button>
-          ) : (
-            <button className="btn btn-primary">Save</button>
-          )}
-        </form>
-      </div>
-      {showAlert ? (
-        <div className="alert alert-info" role="alert">
-          {alert}
-        </div>
-      ) : null}
-    </div>
+                {loading ? (
+                  <button className="btn btn-primary" type="button" disabled>
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                    Loading...
+                  </button>
+                ) : (
+                  <button className="btn btn-primary">Save</button>
+                )}
+              </form>
+            </div>
+            {showAlert ? (
+              <div className="alert alert-info" role="alert">
+                {alert}
+              </div>
+            ) : null}
+          </div>
+        </>
+      )}
+    </>
   );
 };
